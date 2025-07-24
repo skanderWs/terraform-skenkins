84778ff51f490b27d6d7ad59ef45c7fe8950f45b
SkanderGroupe <skanderws1@gmail.com>
2025-07-24 01:20:02 +0100

    Commit the project

--- main.tf
+++
@@ -1,9 +0,0 @@
-module "networking" {
-    source = "./networking"
-    vpc_cidr = var.vpc_cidr
-    vpc_name = var.vpc_name
-    cidr_public_subnet = var.cidr_public_subnet
-    cidr_private_subnet = var.cidr_private_subnet
-    eu_availability_zone = var.eu_availability_zone
-
-}
--- provider.tf
+++
@@ -1,4 +0,0 @@
-provider "aws" {
-  region                   = "eu-west-1"
-  shared_credentials_files = ["/home/skander/.aws/credentials"]
-}
